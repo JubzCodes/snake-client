73fe2a4daefaa4c4e10bd3e372d6512621d66300
jaden <jadenbusiness@hotmail.com>
2021-12-02 04:02:00 +0000

    made input.js

--- play.js
+++ play.js
@@ -1,25 +1,10 @@
 const net = require("net");
 const { connect } = require("./client")
+const { setupInput } = require("./input");
 
 
 console.log("Connecting ...");
 connect();
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  const handleUserInput = function () {
-  stdin.on('data', (key) => {
-  //process.stdout.write('.');
-  if (key === '\u0003') {
-  process.exit();
-  }
-  });
-  };
-  stdin.on("data", handleUserInput);
 
-  return stdin;
-};
 setupInput();
