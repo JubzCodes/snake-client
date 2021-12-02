9d88c1506032de5855aadbf5ff61ab0c68501746
jaden <jadenbusiness@hotmail.com>
2021-12-02 03:42:46 +0000

    handle user input

--- play.js
+++ play.js
@@ -5,3 +5,21 @@ const { connect } = require("./client")
 console.log("Connecting ...");
 connect();
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  const handleUserInput = function () {
+  stdin.on('data', (key) => {
+  //process.stdout.write('.');
+  if (key === '\u0003') {
+  process.exit();
+  }
+  });
+  };
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+setupInput();
