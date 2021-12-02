490b080244144ac7cf528899079cbc1e8ec8438b
jaden <jadenbusiness@hotmail.com>
2021-12-02 01:59:56 +0000

    client setup

---
+++ play.js
@@ -0,0 +1,24 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: '165.227.47.243',
+    port: 50541,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+  console.log("Successfully connected to the game server!")
+});
+conn.on("data", (data) => {
+  console.log(data)
+});
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
